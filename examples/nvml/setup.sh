673683bb2c6f61a7431b4c7b95e0491f8cc8da71
Nick Baker <ndbaker1@outlook.com>
2026-01-24 13:33:23 -0800

    put error logs together in ci

--- examples/nvml/setup.sh
+++ examples/nvml/setup.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# download nvidia-smi.
 wget https://developer.download.nvidia.com/compute/nvidia-driver/redist/nvidia_driver/linux-x86_64/nvidia_driver-linux-x86_64-590.48.01-archive.tar.xz
 xz -d nvidia_driver-linux-x86_64-590.48.01-archive.tar.xz
 tar xvf nvidia_driver-linux-x86_64-590.48.01-archive.tar \
