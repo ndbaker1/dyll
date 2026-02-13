bcdb491f80f595990ba0546c5445059ed125687e
Nick Baker <nbaker@coreweave.com>
2026-02-13 00:20:50 -0800

    fix: ci cannot use tty

--- examples/nvml/run.sh
+++ examples/nvml/run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # the host can run using a local binary like nvidia-smi.
-docker run --rm -it -e LD_LIBRARY_PATH=. libnvidia-ml-so:latest $@
+docker run --rm -e LD_LIBRARY_PATH=. libnvidia-ml-so:latest $@
